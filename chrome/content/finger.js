arlolra/ctypes-otr

commit 9a92ff3f9af2ad5533aaacd6fc1a4cd753984104

Use protocol name in user facing strings
 * Trac 16490

chrome/content/otr.js
@@ -64,6 +64,20 @@ function isOnline(conv) {
   return ret;
 }
 
+// Use the protocol name in user facing strings. See trac #16490
+let names;
+function protocolName(aNormalizedName) {
+  if (!names) {
+    names = new Map();
+    let protocols = Services.core.getProtocols();
+    while (protocols.hasMoreElements()) {
+      let protocol = protocols.getNext();
+      names.set(protocol.normalizedName, protocol.name);
+    }
+  }
+  return names.get(aNormalizedName) || aNormalizedName;
+}
+
 // libotr context wrapper
 
 function Context(context) {
@@ -144,6 +158,8 @@ let otr = {
   log: function(msg) {
     this.notifyObservers(msg, "otr:log");
   },
+
+  protocolName: protocolName,
 
   setPolicy: function(requireEncryption) {
     this.policy = requireEncryption
@@ -292,7 +308,7 @@ let otr = {
           fingerprint: otr.hashToHuman(fingerprint),
           screenname: wContext.username,
           account: wContext.account,
-          protocol: wContext.protocol,
+          protocol: otr.protocolName(wContext.protocol),
           trust: trust,
           status: used ? getStatus(best_level) : _("trust.unused"),
           purge: false,

chrome/content/prefs.js
@@ -7,17 +7,13 @@ Cu.import("chrome://otr/content/otr.js");
 const fingerDialog = "chrome://otr/content/finger.xul";
 const privDialog = "chrome://otr/content/priv.xul";
 
-XPCOMUtils.defineLazyGetter(this, "_", function()
-  l10nHelper("chrome://otr/locale/auth.properties")
-);
-
 let otrPref = {
 
   onload: function() {
     let accountList = document.getElementById("accountlist");
     for (let acc in this.getAccounts()) {
       let menuItem = accountList.appendItem(
-        `${acc.normalizedName} (${acc.protocol.normalizedName})`,
+        `${acc.normalizedName} (${otr.protocolName(acc.protocol.normalizedName)})`,
         acc.id
       );
       if (!accountList.selectedItem) {

chrome/content/priv.js
@@ -13,7 +13,7 @@ let otrPriv = {
   onload: function() {
     let args = window.arguments[0].wrappedJSObject;
     let priv = document.getElementById("priv");
-    priv.textContent = _("priv.account", args.account, args.protocol);
+    priv.textContent = _("priv.account", args.account, otr.protocolName(args.protocol));
     otr.generatePrivateKey(args.account, args.protocol).then(function(success) {
       if (!success)
         throw new Error("Generating key failed!")

commit 9c80ec22c6faaabf7c6792bff2d29577a3752d2c

Fix verifying from known fingerprint manager
 * Broken by 9a92ff3f9af2ad5533aaacd6fc1a4cd753984104

chrome/content/finger.js
@@ -23,6 +23,8 @@ let fingerTreeView = {
     switch(column.id) {
       case "verified":
         return finger.trust ? _("verified.yes") : _("verified.no");
+      case "protocol":
+        return otr.protocolName(finger.protocol);
       default:
         return finger[column.id] || "";
     }

chrome/content/otr.js
@@ -308,7 +308,7 @@ let otr = {
           fingerprint: otr.hashToHuman(fingerprint),
           screenname: wContext.username,
           account: wContext.account,
-          protocol: otr.protocolName(wContext.protocol),
+          protocol: wContext.protocol,
           trust: trust,
           status: used ? getStatus(best_level) : _("trust.unused"),
           purge: false,